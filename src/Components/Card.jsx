d73af2dbb861b09dfb95026896993be93204b790
Anna Klimagina <anklimagina@gmail.com>
2024-06-07 09:40:41 +0300

    add useState

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,26 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../Components/card.module.css';
 
 function Card(props) {
 
-    let transform;
-    if (props.price == 550) {
-        transform = 'scale(1.1)'
-    }
-
+    const [checked, setChecked] = useState(false)
 
     return (
-        <div className={styles.card} style={{ transform: transform }}>
-            <div >
-                <h4 className={styles.card_header} style={{ backgroundColor: props.topColor }}>{props.name}</h4>
-            </div>
-            <div >
-                <p className={styles.card_price} style={{ backgroundColor: props.color }}><span>руб </span>{props.price}<span>/мес</span></p>
-                <p className={styles.card_speed}> до {props.speed} Мбит/сек</p>
-                <p className={styles.card_info}> {props.info}</p>
-            </div>
-        </div >
-    );
+        <>
+            <div className={checked ? `${styles.card} ${styles.transform}` : `${styles.card}`} onClick={() => setChecked(!checked)}>
+
+                <div >
+                    <h4 className={styles.card_header} style={{ backgroundColor: props.topColor }}>{props.name}</h4>
+                </div>
+                <div >
+                    <p className={styles.card_price} style={{ backgroundColor: props.color }}><span>руб </span>{props.price}<span>/мес</span></p>
+                    <p className={styles.card_speed} > до {props.speed} Мбит/сек</p>
+                    <p className={styles.card_info} > {props.info}</p>
+                </div>
+
+            </div >
+        </>);
 }
 
 export default Card
